f35a285f4cd1f96f95df61eb15d8119d215770f4
RayDev <raydev0612@gmail.com>
2023-07-28 14:37:50 -0300

    commit almuerzo2

--- src/Sidebar/Colors/Colors.js
+++ src/Sidebar/Colors/Colors.js
@@ -2,7 +2,32 @@ import './Colors.css'
 
 const Colors = () => {
   return (
-    <div>Colors</div>
+    <div>
+      <label className="sidebar-label-container color-title">
+        <input type="radio" name="test2" />
+        <span className="chekmark"></span>All
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="chekmark"></span>Black
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="chekmark"></span>Blue
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="chekmark"></span>Red
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="chekmark"></span>Green
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="chekmark"></span>White
+      </label>
+    </div>
   )
 }
 
--- src/Sidebar/Price/Price.js
+++ src/Sidebar/Price/Price.js
@@ -1,9 +1,32 @@
-import './Price.css'
+import "./Price.css";
 
 const Price = () => {
   return (
-    <div>Price</div>
-  )
-}
+    <div className="ml">
+      <h2 className="sidebar-title price-title">Price</h2>
 
-export default Price
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="chekmark"></span>All
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="chekmark"></span>$0 - $50
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="chekmark"></span>$50 - $100
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="chekmark"></span>$100 - $150
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="chekmark"></span>Over $150
+      </label>
+    </div>
+  );
+};
+
+export default Price;
